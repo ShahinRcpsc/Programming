6bc61acb49388815de6b1e59cb7d8af930c806c5
ShahinRcpsc <71516989+ShahinRcpsc@users.noreply.github.com>
2020-10-11 12:19:53 +0600

    Update funtion.c

--- funtion.c
+++ funtion.c
@@ -1,1 +1,11 @@
-
+#include <stdio.h>
+int summation(int a, int b) {
+	return  a+b;
+}
+int main() {
+	int a, b, sum;
+	scanf("%d%d", &a, &b);
+	sum = summation(a,b);
+	printf("sum = %d", sum);
+	return 0;
+}
